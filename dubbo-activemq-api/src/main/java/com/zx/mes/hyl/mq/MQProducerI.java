119f522597f441931c3fedaaf7327e71b6abd349
HYL <931614367@qq.com>
2017-09-20 15:02:52 +0800

    增加activemq邮件发送，微软outlook没有正常发送，qq也没有接受到邮件，初步判断是邮件协议配置有问题，代码测试 OK

---
+++ dubbo-activemq-api/src/main/java/com/zx/mes/hyl/mq/MQProducerI.java
@@ -0,0 +1,12 @@
+package com.zx.mes.hyl.mq;
+
+import com.zx.mes.hyl.model.Mail;
+
+/**
+ * Created by Administrator on 2017/9/20.
+ */
+public interface MQProducerI {
+
+    void sendMessage(final Mail mail);
+
+}
